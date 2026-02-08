83c127ca70a9b271c7012e2588ba3904a12341ea
mathFPGAseek <baycool0422@gmail.com>
2026-02-08 13:19:05 -0700

    Cleanup of code; No errors in Sim; FFT 1-D Does not match; Not sure why Adding verf. logic to check 1-D FFT match in transpose; Fix max samples for 1-D FFT match read out of transpose.

--- mem_transpose_module.vhd
+++ mem_transpose_module.vhd
@@ -17,9 +17,9 @@ entity mem_transpose_module is
                   -- := 110 -> DEBUG InvAH  -> {Load (H* x FH(v))}      : trans memory
                   -- := 111 -> DEBUG update -> {Load Grad, Vk}          : trans & vk memory
   generic(
-  	       debug_capture_file_i : integer := 0;
-  	       debug_state_i : in integer := 0;         -- = 0 no write = 1 write
-  	       g_USE_DEBUG_MODE_i : in natural := 0   -- To use COE file
+  	       debug_capture_file_i : integer := 0;     -- = 0 no capture file; =1 capture file
+  	       debug_state_i : in integer := 0;         -- = 0 no write       ; =1 write
+  	       g_USE_DEBUG_MODE_i : in natural := 0     -- = 0 W/R external to this module; To use COE file = 1.
   	     );
   
   Port (
@@ -38,12 +38,7 @@ entity mem_transpose_module is
 end mem_transpose_module;
 
 architecture stub of mem_transpose_module is
---attribute syn_black_box : boolean;
---attribute black_box_pad_pin : string;
---attribute syn_black_box of stub : architecture is true;
---attribute black_box_pad_pin of stub : architecture is "clka,ena,wea[0:0],addra[7:0],dina[79:0],clkb,enb,addrb[7:0],doutb[79:0]";
---attribute x_core_info : string;
---attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_5,Vivado 2022.2";
+
 
 -- For verification and synthesis
 signal data_out_r                   : std_logic_vector( 79 downto 0);
@@ -92,7 +87,6 @@ COMPONENT blk_mem_debug_h_transpose_seq_mem_LL_0 is
 -------------------------------------------------
 
 constant MAX_SAMPLES : integer := 2**8;  -- maximum number of samples in a frame
---constant IP_WIDTH    : integer := 34;
 constant IP_WIDTH    : integer := 34;
 constant MEM_WIDTH   : integer := IP_WIDTH*2 -1;
 type     MEM_ARRAY is array(0 to  MAX_SAMPLES-1,0 to MAX_SAMPLES-1) of std_logic_vector(MEM_WIDTH downto  0);
@@ -116,8 +110,6 @@ constant MAX_SAMPLES_COUNTER_3 : integer := 384; -- 256 + 128 to mask first rd f
 -- counters
 
 signal state_counter_2_r            : integer;
---signal state_counter_2_rr           : integer;
---signal state_counter_2_rrr          : integer;
 signal clear_state_counter_2_d      : std_logic;
 signal clear_state_counter_2_r      : std_logic;
 signal clear_state_counter_2_rr     : std_logic; 
@@ -133,7 +125,6 @@ signal clear_state_counter_1_r      : std_logic; -- no _d because comes from reg
 -- misc for verification
 signal enable_read                  : std_logic;
 signal enable_read_r                : std_logic;
---signal enable_read_rr               : std_logic;
 signal delay_ena                    : std_logic;
 signal falling_valid_event_d        : std_logic;
 signal falling_valid_event_r        : std_logic;
@@ -157,14 +148,6 @@ signal write_control_from_generic   : std_logic_vector( 0 downto 0);
 signal enable_file_capture          : std_logic_vector( 0 downto 0);
 	
 	
--- signals for kludge to mask first read falling edge.
-signal terminal_state_counter_3_d   : std_logic;
-signal terminal_state_counter_3_r   : std_logic;
-
-signal clear_state_counter_3_r      : std_logic;
-signal state_counter_3_r            : integer;
-
-
 -- signals for last write to buffer
 signal enable_write                     : std_logic;
 
@@ -183,15 +166,6 @@ signal enable_last_read_rr              : std_logic;
 
 signal falling_edge_for_last_read_d     : std_logic;
 signal falling_edge_for_last_read_r     : std_logic;
-
-signal clear_state_counter_4_d          : std_logic;
-signal clear_state_counter_4_r          : std_logic;
-
-signal enable_state_counter_4_d         : std_logic;
-signal enable_state_counter_4_r         : std_logic;
-
-signal state_counter_4_r                : integer;
-
 
 -- States
   
@@ -287,25 +261,15 @@ signal state_counter_4_r                : integer;
 	   variable result       : result_type;    
 	   variable mem_line_var : line;
 	   variable done         : integer;
-	   --variable k            : integer;
-	   --variable fft_spec     : MEM_ARRAY;
 	   variable data_write_var : bit_vector(67 downto 0);
 	   begin
-	   	 	--for i in  0 to MAX_SAMPLES-1 loop
-	      --   for j in 0 to MAX_SAMPLES-1 loop
-	      --      k := fft_bin_center_addr(j);
-	      --      fft_spec(i,k) := (fft_mem(i,j));
-	      --   end loop;
-	      --end loop;
 	     file_open(write_file,"col_wr_h_mem_vectors.txt",write_mode);
 	     report" File Opened for writing ";
 	          for i in  0 to MAX_SAMPLES-1 loop
 	              for j in 0 to MAX_SAMPLES-1 loop
-	                  --data_write_var := to_bitvector(fft_spec(i,j));
 	                  data_write_var := to_bitvector(fft_mem(i,j));
 	                  write(mem_line_var ,data_write_var);
 	                  writeline(write_file,mem_line_var);                  
-	                  --report" Start writing to file ";
 	              end loop;
 	          end loop;
 	      done := 1;
@@ -331,13 +295,11 @@ begin
   -----------------------------------------
   -- Transpose mem_intf
   -----------------------------------------.	
-  --g_use_u1_no_debug : if debug_state_i = 0 generate -- default condition
- g_use_u1_no_debug : if g_USE_DEBUG_MODE_i = 0 generate -- default condition
+   g_use_u1_no_debug : if g_USE_DEBUG_MODE_i = 0 generate -- default condition
  		
   	u1 : entity work.blk_mem_image_gen_LL_0 
   	PORT MAP ( 
   	clka  => clk_i,                                      --clka : in STD_LOGIC;
-  	--ena   => ena,                                      --ena : in STD_LOGIC;
   	ena   => ena_to_mem_d,                               --ena : in STD_LOGIC;
   	wea   => wea,                                        --wea : in STD_LOGIC_VECTOR ( 0 to 0 );
   	addra => addra,                                      --addra : in STD_LOGIC_VECTOR ( 15 downto 0 );
@@ -500,18 +462,14 @@ end generate g_use_u2_debug_h_transpose_mem;
     begin
       if ( rst_i = '1' ) then
           state_counter_2_r       <=  0;
-          --state_counter_2_rr      <=  0;
-          --state_counter_2_rrr     <=  0;
-      --elsif(clear_state_counter_2_rr = '1') then
+         
       elsif(falling_edge_for_last_read_r = '1') then
           state_counter_2_r       <=  0;
-          --state_counter_2_rr      <=  0;
-          --state_counter_2_rrr     <=  0;
+         
       elsif( clk_i'event and clk_i = '1') then
          if ( falling_valid_event_rrr = '1') then 
           state_counter_2_r       <=  state_counter_2_r + 1;
-          --state_counter_2_rr      <=  state_counter_2_r;
-          --state_counter_2_rrr     <=  state_connter_2_rr;
+          
          end if;
       end if;
   end process state_counter_2;
@@ -606,69 +564,11 @@ end generate g_use_u2_debug_h_transpose_mem;
   		end if;
   end process delay_qualify_state_reg;
   
-  qualify_state_int <= qualify_state_rrrr;
-  
-  ----------------------------------------
- -- KLUDGE !!! : To mask first read falling edge which increments write buffer address( NO data to write at first falling edge)
+  qualify_state_int <= qualify_state_rrrr;       
+            
+              
  ----------------------------------------
-  state_counter_3 : process( clk_i, rst_i,clear_state_counter_3_r)
-    begin
-      if ( rst_i = '1' ) then
-          state_counter_3_r       <=  0;
-          --state_counter_2_rr      <=  0;
-          --state_counter_2_rrr     <=  0;
-      elsif(clear_state_counter_3_r = '1') then
-          state_counter_3_r       <=  0;
-          --state_counter_2_rr      <=  0;
-          --state_counter_2_rrr     <=  0;
-      elsif( clk_i'event and clk_i = '1') then
-          state_counter_3_r       <=  state_counter_3_r + 1;
-          --state_counter_2_rr      <=  state_counter_2_r;
-          --state_counter_2_rrr     <=  state_connter_2_rr;
-        
-      end if;
-  end process state_counter_3;
-  
-  					 ----------------------------------------
- 						 -- Decode terminal count for kludge above
-  					----------------------------------------.
-  					decode_terminal_count_for_counter_3 : process(state_counter_3_r)
-  						begin
-  							if (  state_counter_3_r = MAX_SAMPLES_COUNTER_3 ) then
-  									terminal_state_counter_3_d <= '1';
-  	  					else
-  	  							terminal_state_counter_3_d <= '0';
-  	  					end if;
-            end process decode_terminal_count_for_counter_3;
-            
-             ----------------------------------------
- 						 -- Register above signal
-  					----------------------------------------
-  					register_terminal_count_for_counter_3 : process( clk_i, rst_i)
-  						begin
-  							if ( rst_i = '1') then
-  								terminal_state_counter_3_r <= '0';								
-  							else
-  								terminal_state_counter_3_r <= terminal_state_counter_3_d;
-  						  end if;
-  					end process register_terminal_count_for_counter_3;
-            
-            ----------------------------------------
-            -- Register Latch terminal counter 3
-            ----------------------------------------
-            register_latch_terminal_count_for_counter_3 : process(clk_i, rst_i)
-  	          begin
-  		         if ( rst_i = '1') then
-  			          clear_state_counter_3_r  <=  '0';
-  	          elsif(clk_i'event and clk_i  = '1') then
-  	          	  if(terminal_state_counter_3_r = '1' ) then
-  	  	          	clear_state_counter_3_r  <=  '1';
-  	  	          end if;
-  	          end if;
-            end process register_latch_terminal_count_for_counter_3;
-            
- ----------------------------------------
- -- KLUDGE !!! : Logic for last write to buffer; All Memory controller reads and writes have ended
+ -- Logic for last write to buffer; All Memory controller reads and writes have ended
  ----------------------------------------
  
        -- generate extra write; when generated write falls kick of state to generate buffer addr
@@ -709,7 +609,6 @@ end generate g_use_u2_debug_h_transpose_mem;
  				  ----------------------------------------  	
          st_mach_controller : process(
        	       falling_edge_for_last_write_r,
-       	       --state_counter_4_r,
        	       state_counter_1_r,    	  
        	       ps_controller
             ) begin
@@ -734,7 +633,6 @@ end generate g_use_u2_debug_h_transpose_mem;
                  	
                  	 decoder_st_d <= "10";
                  	 
-                 	 --if( state_counter_4_r = 253) then -- This produces a correct number of writes
                  	   if( state_counter_1_r = 253) then
                  	 	
                  	 	 ns_controller <= state_quies;
@@ -811,59 +709,7 @@ end generate g_use_u2_debug_h_transpose_mem;
   		     end if;
   			
          end process falling_edge_for_last_read_reg_proc;	
-         	
-         				
-         				
-         	st_mach_controller_counter_decode : process(
-         		decoder_st_r) begin
-         			
-         			            case  decoder_st_r is
-         			            	
-         			            	when "01" =>
-         			            			      			            		
-  														clear_state_counter_4_d   <= '1'; 
-  														enable_state_counter_4_d  <= '0';
-  														
-  													when "10" =>
-  																												
-  			                      clear_state_counter_4_d   <= '0'; 
-  			                      enable_state_counter_4_d  <= '1';
-         	
-         	                  when others =>
-  																												
-  			                      clear_state_counter_4_d   <= '1'; 
-  			                      enable_state_counter_4_d  <= '0';       	                  	
-         	                  	
-         	                end case;
-         	end process st_mach_controller_counter_decode;
-         	  	 	
-         	
-         	st_mach_contoller_counter_registers : process(clk_i,rst_i)
-         		
-         		begin
-         			if(rst_i = '1')then       					      			            		
-  							clear_state_counter_4_r   <= '1'; 
-  							enable_state_counter_4_r  <= '0';
-         					
-         		  elsif(clk_i'event and clk_i = '1')then
-         		    clear_state_counter_4_r   <= clear_state_counter_4_d; 
-  							enable_state_counter_4_r  <= enable_state_counter_4_d;
-         		  	
-         		  end if;
-         	end process st_mach_contoller_counter_registers;        	 	
-
-  	       state_counter_4 : process( clk_i, rst_i, clear_state_counter_4_r)
-    					begin
-      					if ( rst_i = '1' ) then
-         						state_counter_4_r       <=  0 ;
-      					elsif( clear_state_counter_4_r = '1') then
-              			state_counter_4_r       <=  0 ;
-      					elsif( clk_i'event and clk_i = '1') then
-         						if ( enable_state_counter_4_r = '1') then
-              			state_counter_4_r       <=  state_counter_4_r + 1;
-         						end if;
-      					end if;
-  					end process state_counter_4;
+    
 
    -----------------------------------------------------------------------
   -- Store read outputs from memory; We are reading an array built by  process record_outputs
@@ -879,18 +725,11 @@ end generate g_use_u2_debug_h_transpose_mem;
   RamProcRawData : process(clk_i,rst_i)
 
     begin
-  	  if ( rst_i = '1' ) then
-         --fft_raw_mem <= (Others => '0'); 
+  	  if ( rst_i = '1' ) then       
          dummy <= '1';
-      --elsif(falling_valid_event_d = '1') then
-      --   --fft_raw_mem <= (Others => '0');
-      --   fft_raw_mem(state_counter_1_r,state_counter_2_r) <= data_out_r;
-  	  --elsif enable_read = '1' then 		
-  	  elsif enable_read_rr = '1' then 		
-   		--fft_raw_mem(state_counter_1_r,state_counter_2_r) <= data_out_r;  				  	  
-  		--fft_raw_mem(state_counter_1_rrr,state_counter_2_rrr) <= data_out_r;  
-  		  fft_raw_mem(state_counter_1_rrr,state_counter_2_r) <= data_out_r(73 downto 40) & data_out_r(33 downto 0);  				  			  
-				  
+      	
+  	  elsif enable_read_rr = '1' then 		  
+  		  fft_raw_mem(state_counter_1_rrr,state_counter_2_r) <= data_out_r(73 downto 40) & data_out_r(33 downto 0);  				  			  				  
 				  
   		end if;
    end process RamProcRawData;  
@@ -899,8 +738,7 @@ end generate g_use_u2_debug_h_transpose_mem;
     begin
   	  if ( rst_i = '1' ) then
          dummy_h_read <= '1';
-     elsif( (enable_read_rr = '1') and (master_mode_i = "00011") )then 			
-   		  --h_read_mem(state_counter_1_rrr,state_counter_2_r) <= data_out_r(73 downto 40) & data_out_r(33 downto 0);
+     elsif( (enable_read_rr = '1') and (master_mode_i = "00011") )then 			   		 
    		    h_read_mem(state_counter_2_r,state_counter_1_rrr) <= data_out_r(73 downto 40) & data_out_r(33 downto 0);  				  			  
   				  			  
   		end if;
@@ -913,13 +751,12 @@ end generate g_use_u2_debug_h_transpose_mem;
       elsif( (enable_read_rr = '1') and (master_mode_i = "00011") )then 			-- enable_read_rr instead of enable_write_rr
      	                                                                      -- because we enable WRITING to this buffer h_write_mem
      	                                                                      -- After we have done a enable_write_rr to the real
-     	                                                                      -- memory in the FPGA
-   		  --h_read_mem(state_counter_1_rrr,state_counter_2_r) <= data_out_r(73 downto 40) & data_out_r(33 downto 0);
+     	                                                                      -- memory in the FPGA.  		  
    		    h_write_mem(state_counter_2_r,state_counter_1_rrr) <= dina(73 downto 40) & dina(33 downto 0); 
    		    	
    	 elsif( (enable_last_read_r = '1') and (master_mode_i = "00011") ) then
    		
-   		    h_write_mem(state_counter_2_r,state_counter_4_r) <= dina(73 downto 40) & dina(33 downto 0); 
+   		    h_write_mem(state_counter_2_r,state_counter_1_rrr) <= dina(73 downto 40) & dina(33 downto 0); 
  				  			  
   				  			  
   	 end if;
@@ -936,9 +773,8 @@ data_read : process(clear_state_counter_2_rr)
   --report " This is a read of one frame
 
   begin
-   if( (clear_state_counter_2_rr  = '1') and (enable_file_capture(0) = '1') ) then -- Have completed MAX_SAMPLE FFT Computations( 1-D)o
-        --write_fft_1d_raw_done <= writeToFileMemRawContents(fft_raw_mem,fft_bin_seq_addr);	
-          write_fft_1d_raw_done <= writeToFileMemRawContents(fft_raw_mem);	
+   if( (clear_state_counter_2_rr  = '1') and (enable_file_capture(0) = '1') ) then -- Have completed MAX_SAMPLE FFT Computations( 1-D)
+       write_fft_1d_raw_done <= writeToFileMemRawContents(fft_raw_mem);	
        report "Module: Mem Transpose -> Done Reads for one frame";
    end if;
 end process data_read;
