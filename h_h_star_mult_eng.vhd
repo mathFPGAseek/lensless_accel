5778801b9db4caa22611b68aeee2c17e37fc313e
mathFPGAseek <baycool0422@gmail.com>
2026-06-27 11:33:35 -0700

    Not functionally simulated Edits: Revised build tcl and took out FP IP( This is temp for first SW test; THey were causing syn issues)

--- h_h_star_mult_eng.vhd
+++ h_h_star_mult_eng.vhd
@@ -35,7 +35,7 @@ USE xil_defaultlib.all;
 
 entity h_h_star_mult_eng is
   generic(
---	     g_USE_DEBUG_i  : in natural := 1);
+
          g_USE_DEBUG_MODE_i : in natural:= 0
          );
          
@@ -134,14 +134,6 @@ signal port_2_u6_rdy                   : std_logic;
 
 signal addr_int                        : std_logic_vector(7 downto 0);
 
---signal s_axis_a_tlast_int            : std_logic;
---signal s_axis_b_tlast_int            : std_logic;
-
---signal m_axis_dout_tlast_int         : std_logic;
-
---signal addr_int                      : std_logic_vector(7 downto 0);
-	
---signal not_reset                     : std_logic;
 
 constant PAD_EIGHT_ZEROS : std_logic_vector(7 downto 0) := (others=> '0');
 constant PAD_NINE_ZEROS  : std_logic_vector(8 downto 0) := (others=> '0');
@@ -155,7 +147,7 @@ begin
     -- H H_star St mach contoller
     -----------------------------------------	 
     U0 : entity xil_defaultlib.h_hstar_inbound_state_machine_controller 
-    --U0 : entity work.h_hstar_inbound_state_machine_controller 
+    
     GENERIC MAP(
     	   g_USE_DEBUG_MODE_i => g_USE_DEBUG_MODE_i
     )           
@@ -178,131 +170,147 @@ begin
     );
  
 
---s_axis_b_tlast_int <=  s_axis_b_tlast_int; 
---addr_out_o         <= "000000000" & addr_int;
---not_reset          <= not( rst_i);	
 
---U1 : entity work.cmpy_0 
---PORT MAP ( 
---    aclk                  =>    clk_i, --: in STD_LOGIC;
---    aresetn               =>    not_reset, --: in STD_LOGIC;
---    s_axis_a_tvalid       =>    port_1_valid_in_i, --: in STD_LOGIC;
---    s_axis_a_tlast        =>    s_axis_a_tlast_int, --: in STD_LOGIC;
---    s_axis_a_tdata        =>    port_1_data_in_i, --: in STD_LOGIC_VECTOR ( 79 downto 0 );
---    s_axis_b_tvalid       =>    port_2_valid_in_i, --: in STD_LOGIC;
---    s_axis_b_tlast        =>    s_axis_b_tlast_int, --: in STD_LOGIC;
---    s_axis_b_tdata        =>    port_2_data_in_i, --: in STD_LOGIC_VECTOR ( 79 downto 0 );
---    m_axis_dout_tvalid    =>    valid_out_o, --: out STD_LOGIC;
---    m_axis_dout_tlast     =>    m_axis_dout_tlast_int, --: out STD_LOGIC;
---    m_axis_dout_tdata     =>    data_out_o --: out STD_LOGIC_VECTOR ( 79 downto 0 ).
---  );
 
 --   Port 1 : A + Bi.
 --   Port 2 : C + Di
 --   (AC - BD) Re + (AD + BC) Im
 
-U1: entity xil_defaultlib.fp_mult -- A(Re)*C(Re)
-  PORT MAP ( 
-  aclk                   => clk_i,    --- aclk : in STD_LOGIC;
-  aresetn                => not(rst_i),
-  s_axis_a_tvalid        => port_1_valid_in_i,         -- s_axis_a_tvalid : in STD_LOGIC;
-  s_axis_a_tready        => port_1_u1_rdy,         -- s_axis_a_tready : out STD_LOGIC;
-  s_axis_a_tdata         => port_1_data_in_i(31 downto 0),         -- s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-  s_axis_b_tvalid        => port_2_valid_in_i,         -- s_axis_b_tvalid : in STD_LOGIC;
-  s_axis_b_tready        => port_2_u1_rdy,         -- s_axis_b_tready : out STD_LOGIC;
-  s_axis_b_tdata         => port_2_data_in_i(31 downto 0),         -- s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-  m_axis_result_tvalid   => ac_data_valid,         -- m_axis_result_tvalid : out STD_LOGIC;
-  m_axis_result_tready   => '1',         -- m_axis_result_tready : in STD_LOGIC;
-  m_axis_result_tdata    => ac_data         -- m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
-  );
+--U1: entity xil_defaultlib.fp_mult -- A(Re)*C(Re)
+--  PORT MAP ( 
+--  aclk                   => clk_i,    --- aclk : in STD_LOGIC;
+--  aresetn                => not(rst_i),
+--  s_axis_a_tvalid        => port_1_valid_in_i,         -- s_axis_a_tvalid : in STD_LOGIC;
+--  s_axis_a_tready        => port_1_u1_rdy,         -- s_axis_a_tready : out STD_LOGIC;
+--  s_axis_a_tdata         => port_1_data_in_i(31 downto 0),         -- s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+--  s_axis_b_tvalid        => port_2_valid_in_i,         -- s_axis_b_tvalid : in STD_LOGIC;
+--  s_axis_b_tready        => port_2_u1_rdy,         -- s_axis_b_tready : out STD_LOGIC;
+--  s_axis_b_tdata         => port_2_data_in_i(31 downto 0),         -- s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+--  m_axis_result_tvalid   => ac_data_valid,         -- m_axis_result_tvalid : out STD_LOGIC;
+--  m_axis_result_tready   => '1',         -- m_axis_result_tready : in STD_LOGIC;
+--  m_axis_result_tdata    => ac_data         -- m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+--  );
+  
+  port_1_u1_rdy <= '0';
+  port_2_u1_rdy <= '0';
+  ac_data <= (others => '0');
+  ac_data_valid <= '0';
+  
  
 
-U2: entity xil_defaultlib.fp_mult -- B(Im)*D(Im)
-  PORT MAP ( 
-  aclk                   => clk_i,    --- aclk : in STD_LOGIC;
-  aresetn                => not(rst_i),
-  s_axis_a_tvalid        => port_1_valid_in_i,         -- s_axis_a_tvalid : in STD_LOGIC;
-  s_axis_a_tready        => port_1_u2_rdy,         -- s_axis_a_tready : out STD_LOGIC;
-  s_axis_a_tdata         => port_1_data_in_i(71 downto 40),         -- s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-  s_axis_b_tvalid        => port_2_valid_in_i,         -- s_axis_b_tvalid : in STD_LOGIC;
-  s_axis_b_tready        => port_2_u2_rdy,         -- s_axis_b_tready : out STD_LOGIC;
-  s_axis_b_tdata         => port_2_data_in_i(71 downto 40),         -- s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-  m_axis_result_tvalid   => bd_data_valid,         -- m_axis_result_tvalid : out STD_LOGIC;
-  m_axis_result_tready   => '1',         -- m_axis_result_tready : in STD_LOGIC;
-  m_axis_result_tdata    => bd_data         -- m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
-  ); 
-
-
-
-U3: entity xil_defaultlib.fp_mult -- A(Re)*D(Im).
-  PORT MAP ( 
-  aclk                   => clk_i,    --- aclk : in STD_LOGIC;
-  aresetn                => not(rst_i),
-  s_axis_a_tvalid        => port_1_valid_in_i,         -- s_axis_a_tvalid : in STD_LOGIC;
-  s_axis_a_tready        => port_1_u3_rdy,         -- s_axis_a_tready : out STD_LOGIC;
-  s_axis_a_tdata         => port_1_data_in_i(31 downto 0),         -- s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-  s_axis_b_tvalid        => port_2_valid_in_i,         -- s_axis_b_tvalid : in STD_LOGIC;
-  s_axis_b_tready        => port_2_u3_rdy,         -- s_axis_b_tready : out STD_LOGIC;
-  s_axis_b_tdata         => port_2_data_in_i(71 downto 40),         -- s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-  m_axis_result_tvalid   => ad_data_valid,         -- m_axis_result_tvalid : out STD_LOGIC;
-  m_axis_result_tready   => '1',         -- m_axis_result_tready : in STD_LOGIC;
-  m_axis_result_tdata    => ad_data         -- m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
-  );
+--U2: entity xil_defaultlib.fp_mult -- B(Im)*D(Im)
+--  PORT MAP ( 
+--  aclk                   => clk_i,    --- aclk : in STD_LOGIC;
+--  aresetn                => not(rst_i),
+--  s_axis_a_tvalid        => port_1_valid_in_i,         -- s_axis_a_tvalid : in STD_LOGIC;
+--  s_axis_a_tready        => port_1_u2_rdy,         -- s_axis_a_tready : out STD_LOGIC;
+--  s_axis_a_tdata         => port_1_data_in_i(71 downto 40),         -- s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+--  s_axis_b_tvalid        => port_2_valid_in_i,         -- s_axis_b_tvalid : in STD_LOGIC;
+--  s_axis_b_tready        => port_2_u2_rdy,         -- s_axis_b_tready : out STD_LOGIC;
+--  s_axis_b_tdata         => port_2_data_in_i(71 downto 40),         -- s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+--  m_axis_result_tvalid   => bd_data_valid,         -- m_axis_result_tvalid : out STD_LOGIC;
+--  m_axis_result_tready   => '1',         -- m_axis_result_tready : in STD_LOGIC;
+--  m_axis_result_tdata    => bd_data         -- m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+--  ); 
   
   
-U4: entity xil_defaultlib.fp_mult -- B(Im)*C(Re)
-  PORT MAP ( 
-  aclk                   => clk_i,    --- aclk : in STD_LOGIC;
-  aresetn                => not(rst_i),
-  s_axis_a_tvalid        => port_1_valid_in_i,         -- s_axis_a_tvalid : in STD_LOGIC;
-  s_axis_a_tready        => port_1_u4_rdy,         -- s_axis_a_tready : out STD_LOGIC;
-  s_axis_a_tdata         => port_1_data_in_i(71 downto 40),         -- s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-  s_axis_b_tvalid        => port_2_valid_in_i,         -- s_axis_b_tvalid : in STD_LOGIC;
-  s_axis_b_tready        => port_2_u4_rdy,         -- s_axis_b_tready : out STD_LOGIC;
-  s_axis_b_tdata         => port_2_data_in_i(31 downto 0),         -- s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-  m_axis_result_tvalid   => bc_data_valid,         -- m_axis_result_tvalid : out STD_LOGIC;
-  m_axis_result_tready   => '1',         -- m_axis_result_tready : in STD_LOGIC;
-  m_axis_result_tdata    => bc_data         -- m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
-  );                                         
+  port_1_u2_rdy <= '0';
+  port_2_u2_rdy <= '0';
+  bd_data <= (others => '0');
+  bd_data_valid <= '0';
+
+
+
+--U3: entity xil_defaultlib.fp_mult -- A(Re)*D(Im).
+--  PORT MAP ( 
+--  aclk                   => clk_i,    --- aclk : in STD_LOGIC;
+--  aresetn                => not(rst_i),
+--  s_axis_a_tvalid        => port_1_valid_in_i,         -- s_axis_a_tvalid : in STD_LOGIC;
+--  s_axis_a_tready        => port_1_u3_rdy,         -- s_axis_a_tready : out STD_LOGIC;
+--  s_axis_a_tdata         => port_1_data_in_i(31 downto 0),         -- s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+--  s_axis_b_tvalid        => port_2_valid_in_i,         -- s_axis_b_tvalid : in STD_LOGIC;
+--  s_axis_b_tready        => port_2_u3_rdy,         -- s_axis_b_tready : out STD_LOGIC;
+--  s_axis_b_tdata         => port_2_data_in_i(71 downto 40),         -- s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+--  m_axis_result_tvalid   => ad_data_valid,         -- m_axis_result_tvalid : out STD_LOGIC;
+--  m_axis_result_tready   => '1',         -- m_axis_result_tready : in STD_LOGIC;
+--  m_axis_result_tdata    => ad_data         -- m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+--  );
+
+
+  port_1_u3_rdy <= '0';
+  port_2_u3_rdy <= '0';
+  ad_data <= (others => '0');
+  ad_data_valid <= '0';
+  
+  
+--U4: entity xil_defaultlib.fp_mult -- B(Im)*C(Re)
+--  PORT MAP ( 
+--  aclk                   => clk_i,    --- aclk : in STD_LOGIC;
+--  aresetn                => not(rst_i),
+--  s_axis_a_tvalid        => port_1_valid_in_i,         -- s_axis_a_tvalid : in STD_LOGIC;
+--  s_axis_a_tready        => port_1_u4_rdy,         -- s_axis_a_tready : out STD_LOGIC;
+--  s_axis_a_tdata         => port_1_data_in_i(71 downto 40),         -- s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+--  s_axis_b_tvalid        => port_2_valid_in_i,         -- s_axis_b_tvalid : in STD_LOGIC;
+--  s_axis_b_tready        => port_2_u4_rdy,         -- s_axis_b_tready : out STD_LOGIC;
+--  s_axis_b_tdata         => port_2_data_in_i(31 downto 0),         -- s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+--  m_axis_result_tvalid   => bc_data_valid,         -- m_axis_result_tvalid : out STD_LOGIC;
+--  m_axis_result_tready   => '1',         -- m_axis_result_tready : in STD_LOGIC;
+--  m_axis_result_tdata    => bc_data         -- m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+--  ); 
+
+  port_1_u4_rdy <= '0';
+  port_2_u4_rdy <= '0';
+  bc_data <= (others => '0');
+  bc_data_valid <= '0';                                       
 
  -----------------------------------------	                                         
  -- (AC - BD ) Re
  -----------------------------------------.	
 
- U5: entity xil_defaultlib.fp_sub
-  PORT MAP ( 
-   aclk                   => clk_i, --: in STD_LOGIC;
-   s_axis_a_tvalid        => sub_re_input_valid_r,--s_axis_a_tvalid : in STD_LOGIC;
-   s_axis_a_tready        => port_1_u5_rdy,--s_axis_a_tready : out STD_LOGIC;
-   s_axis_a_tdata         => ac_data,--s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-   s_axis_b_tvalid        => sub_re_input_valid_r,--s_axis_b_tvalid : in STD_LOGIC;
-   s_axis_b_tready        => port_2_u5_rdy,--s_axis_b_tready : out STD_LOGIC;
-   s_axis_b_tdata         => bd_data,--s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-   m_axis_result_tvalid   => ac_minus_bd_valid_d,--m_axis_result_tvalid : out STD_LOGIC;
-   m_axis_result_tready   => '1',--m_axis_result_tready : in STD_LOGIC;
-   m_axis_result_tdata    => ac_minus_bd_data_d--m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
-  ); 
+ --U5: entity xil_defaultlib.fp_sub
+ -- PORT MAP ( 
+ --  aclk                   => clk_i, --: in STD_LOGIC;
+ --  s_axis_a_tvalid        => sub_re_input_valid_r,--s_axis_a_tvalid : in STD_LOGIC;
+ --  s_axis_a_tready        => port_1_u5_rdy,--s_axis_a_tready : out STD_LOGIC;
+ --  s_axis_a_tdata         => ac_data,--s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+ --  s_axis_b_tvalid        => sub_re_input_valid_r,--s_axis_b_tvalid : in STD_LOGIC;
+ --  s_axis_b_tready        => port_2_u5_rdy,--s_axis_b_tready : out STD_LOGIC;
+ --  s_axis_b_tdata         => bd_data,--s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+ --  m_axis_result_tvalid   => ac_minus_bd_valid_d,--m_axis_result_tvalid : out STD_LOGIC;
+ --  m_axis_result_tready   => '1',--m_axis_result_tready : in STD_LOGIC;
+ --  m_axis_result_tdata    => ac_minus_bd_data_d--m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+ -- ); 
+ 
   
+  port_1_u5_rdy <= '0';
+  port_2_u5_rdy <= '0';
+  ac_minus_bd_data_d <= (others => '0');
+  ac_minus_bd_valid_d <= '0';  
   
  -----------------------------------------	                                         
  -- (AD + BC ) Im
  -----------------------------------------
  
- U6: entity xil_defaultlib.fp_add
-  PORT MAP ( 
-   aclk                   => clk_i, --: in STD_LOGIC;
-   s_axis_a_tvalid        => add_im_input_valid_r,--s_axis_a_tvalid : in STD_LOGIC;
-   s_axis_a_tready        => port_1_u6_rdy,--s_axis_a_tready : out STD_LOGIC;
-   s_axis_a_tdata         => ad_data,--s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-   s_axis_b_tvalid        => add_im_input_valid_r,--s_axis_b_tvalid : in STD_LOGIC;
-   s_axis_b_tready        => port_2_u6_rdy,--s_axis_b_tready : out STD_LOGIC;
-   s_axis_b_tdata         => bc_data,--s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-   m_axis_result_tvalid   => ad_plus_bc_valid_d,--m_axis_result_tvalid : out STD_LOGIC;
-   m_axis_result_tready   => '1',--m_axis_result_tready : in STD_LOGIC;
-   m_axis_result_tdata    => ad_plus_bc_data_d--m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
-  ); 
+ --U6: entity xil_defaultlib.fp_add
+ -- PORT MAP ( 
+ --  aclk                   => clk_i, --: in STD_LOGIC;
+ --  s_axis_a_tvalid        => add_im_input_valid_r,--s_axis_a_tvalid : in STD_LOGIC;
+ --  s_axis_a_tready        => port_1_u6_rdy,--s_axis_a_tready : out STD_LOGIC;
+ --  s_axis_a_tdata         => ad_data,--s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+ --  s_axis_b_tvalid        => add_im_input_valid_r,--s_axis_b_tvalid : in STD_LOGIC;
+ --  s_axis_b_tready        => port_2_u6_rdy,--s_axis_b_tready : out STD_LOGIC;
+ --  s_axis_b_tdata         => bc_data,--s_axis_b_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+ --  m_axis_result_tvalid   => ad_plus_bc_valid_d,--m_axis_result_tvalid : out STD_LOGIC;
+ --  m_axis_result_tready   => '1',--m_axis_result_tready : in STD_LOGIC;
+ --  m_axis_result_tdata    => ad_plus_bc_data_d--m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+ -- ); 
   
- 
+  
+  port_1_u6_rdy <= '0';
+  port_2_u6_rdy <= '0';
+  ad_plus_bc_data_d<= (others => '0');
+  ad_plus_bc_valid_d <= '0'; 
+  
   
   sub_re_input_valid_d <= ac_data_valid and bd_data_valid;
   add_im_input_valid_d <= ad_data_valid and bc_data_valid;
--- scripts/build_project.tcl
+++ scripts/build_project.tcl
@@ -37,7 +37,7 @@ set coe_dir    [file normalize [file join $repo_root "coe"]]
 set ip_src_dir [file normalize [file join $repo_root "ip_src"]]
 
 # Control whether to run synthesis after project/IP creation
-set run_synth 1
+set run_synth 0
 
 # ------------------------------------------------------------
 # Clean/create build area
@@ -65,6 +65,15 @@ puts "  Part    = $part_name"
 
 create_project $proj_name $proj_dir -part $part_name -force
 
+puts "current_project = <[current_project]>"
+puts "project NAME    = <[get_property NAME [current_project]]>"
+puts "project DIR     = <[get_property DIRECTORY [current_project]]>"
+puts "project PART    = <[get_property PART [current_project]]>"
+
+#puts "SAVE CHECK: after create_project"
+#save_project
+#puts "PASSED: after create_project"
+
 set_property target_language VHDL [current_project]
 set_property simulator_language Mixed [current_project]
 
@@ -75,12 +84,7 @@ set_property simulator_language Mixed [current_project]
 puts ""
 puts "Adding RTL files from: $rtl_dir"
 
-set rtl_files [glob -nocomplain \
-    [file join $rtl_dir "*.vhd"] \
-    [file join $rtl_dir "*.vhdl"] \
-    [file join $rtl_dir "*.sv"] \
-    [file join $rtl_dir "*.v"] \
-]
+set rtl_files [glob -nocomplain [file join $rtl_dir "*.vhd"]]
 
 if {[llength $rtl_files] == 0} {
     puts "WARNING: No RTL files found in $rtl_dir"
@@ -89,8 +93,13 @@ if {[llength $rtl_files] == 0} {
         puts "  RTL: $f"
     }
 
-    add_files -fileset sources_1 $rtl_files
+    #add_files -fileset sources_1 $rtl_files
+    import_files -fileset sources_1 $rtl_files
 }
+
+#puts "SAVE CHECK: after add RTL"
+#save_project
+#puts "PASSED: after add RTL"
 
 # ------------------------------------------------------------
 # Add constraints by reference
@@ -111,6 +120,10 @@ if {[llength $xdc_files] == 0} {
     add_files -fileset constrs_1 $xdc_files
 }
 
+#puts "SAVE CHECK: after add XDC"
+#save_project
+#puts "PASSED: after add XDC"
+
 # ------------------------------------------------------------
 # Source existing IP creation script
 # ------------------------------------------------------------
@@ -128,6 +141,10 @@ set ::xdc_dir    $xdc_dir
 
 source [file join $script_dir "create_ip.tcl"]
 
+#puts "SAVE CHECK: after create_ip"
+#save_project
+#puts "PASSED: after create_ip"
+
 # ------------------------------------------------------------
 # Update compile order and save project
 # ------------------------------------------------------------
@@ -135,9 +152,56 @@ source [file join $script_dir "create_ip.tcl"]
 puts ""
 puts "Updating compile order..."
 
+puts " DBG: before update_copile_order"
 update_compile_order -fileset sources_1
+puts " DBG: after update_copile_order"
 
-save_project
+puts "DBG: current_project = <[current_project]>"
+puts "DBG: project name    = <[get_property NAME [current_project]]>"
+puts "DBG: project dir     = <[get_property DIRECTORY [current_project]]>"
+
+puts ""
+puts "DBG: IPs currently known:"
+foreach ip [get_ips -quiet] {
+    puts "  IP = <$ip>"
+    puts "     NAME      = <[get_property NAME $ip]>"
+    puts "     IPDEF     = <[get_property IPDEF $ip]>"
+}
+
+puts ""
+puts "Filesets:"
+foreach fs [get_filesets -quiet] {
+    puts "  fileset = <$fs>"
+    puts "    NAME = <[get_property NAME $fs]>"
+}
+
+puts ""
+puts "Source files:"
+foreach f [get_files -quiet -of_objects [get_filesets sources_1]] {
+    puts "  file = <$f>"
+    puts "    NAME      = <[get_property NAME $f]>"
+    puts "    FILE_TYPE = <[get_property FILE_TYPE $f]>"
+}
+
+puts ""
+puts "UNKNOWN files in sources_1:"
+foreach f [get_files -quiet -of_objects [get_filesets sources_1]] {
+    set ft [get_property FILE_TYPE $f]
+    if {$ft eq "UNKNOWN"} {
+        puts "  FILE = <$f>"
+        puts "  NAME = <[get_property NAME $f]>"
+    }
+}
+
+#puts " DBG: before save_project"
+#save_project
+#puts " DBG: after save_project"
+
+#create_project fa_abs_create_only $proj_dir -part xczu7ev-ffvc1156-2-e -force
+
+puts "Before close_project"
+close_project
+puts "After close_project"
 
 puts ""
 puts "Project creation and IP generation complete."
